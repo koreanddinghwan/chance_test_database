1c3418267a9793df15268b5507cbad788f6fe6b3
myukang <myunghwan0421@gmail.com>
2023-01-11 00:35:48 +0900

    FEAT: update configuration file

---
+++ config.sh
@@ -0,0 +1,16 @@
+read -p "please enter your database host: " host
+read -p "port: " port
+read -p "your database name: " database
+read -p "user name(this user should have access permission)" user
+read -p "user password: " password
+
+echo 
+"
+	HOST=${host}
+	PORT=${port}
+	DATABASE=${database}
+	DBUSER=${user}
+	PASSWORD=${password}
+" > .env.dev
+
+echo "Finished your configuration~!"
